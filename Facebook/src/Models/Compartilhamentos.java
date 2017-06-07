941bdbc8a4c170ec6b2d0eefd2715f1453e9960d
Alexsander Magnum <Alexsander Magnum>
2017-06-07 01:49:03 -0300

    Atv Facebook

---
+++ Facebook/src/Models/Compartilhamentos.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class Compartilhamentos {
+
+	public Post getPostCompartilhado() {
+		return postCompartilhado;
+	}
+
+	public void setPostCompartilhado(Post postCompartilhado) {
+		this.postCompartilhado = postCompartilhado;
+	}
+
+	Post postCompartilhado;
+
+}
